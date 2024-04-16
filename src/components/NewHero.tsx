8ae43c6d88d6ac7fe1b9f2a0722eef7fa85b18de
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-04-16 12:42:50 +0530

    continue fixing sparkles

--- src/components/NewHero.tsx
+++ src/components/NewHero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { SparklesCore } from "./ui/sparkles";
+import { SparklesCore } from "@/components/ui/sparkles";
 
 
 
